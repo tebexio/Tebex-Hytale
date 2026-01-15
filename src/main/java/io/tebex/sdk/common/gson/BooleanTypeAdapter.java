6ba151c2ca85f50fcb523f1fd1448eb524591934
Dusty A <WildBamaBoy@users.noreply.github.com>
2026-01-15 12:50:06 -0600

    docs, strict string handling if "1" or "0"

--- src/main/java/io/tebex/sdk/common/gson/BooleanTypeAdapter.java
+++ src/main/java/io/tebex/sdk/common/gson/BooleanTypeAdapter.java
@@ -7,6 +7,10 @@ import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
 
+/**
+ * Some APIs can return booleans as numbers (0/1,"0"/"1") instead of true/false.
+ * GSON would throw JsonSyntaxException when deserializing package data, ex. `create_giftcard`
+ */
 public final class BooleanTypeAdapter extends TypeAdapter<Boolean> {
 
     @Override
@@ -23,13 +27,21 @@ public final class BooleanTypeAdapter extends TypeAdapter<Boolean> {
         return switch (in.peek()) {
             case BOOLEAN -> in.nextBoolean();
             case NUMBER -> in.nextInt() != 0;
-            case STRING -> Boolean.parseBoolean(in.nextString());
+            case STRING -> {
+                var str = in.nextString();
+                if (str.equalsIgnoreCase("true") || str.equalsIgnoreCase("1")) yield true;
+                else if (str.equalsIgnoreCase("false") || str.equalsIgnoreCase("0")) yield false;
+                else {
+                    var message = (str.length() > 32 ? (str.substring(0,32) + "...(truncated)") : str);
+                    throw new JsonParseException("Invalid boolean value '" + message + "'");
+                }
+            }
             case NULL -> {
                 in.nextNull();
                 yield false;
             }
             default -> throw new JsonParseException(
-                    "Invalid boolean value: " + in.peek()
+                    "Invalid boolean value of type: " + in.peek() + " at " + in.getPath()
             );
         };
     }
